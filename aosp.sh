c1a3d8ba899f1e265db2772a1fe5fcbb990b05b6
kitw4y <91876095+kitw4y@users.noreply.github.com>
2024-02-12 21:58:12 +0700

    Update aosp.sh

--- aosp.sh
+++ aosp.sh
@@ -1,7 +1,6 @@
 # sync rom
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
 git clone https://github.com/kitw4y/local_manifest.git --depth 1 -b RisingOS-14 .repo/local_manifests
-rm -rf prebuilts/clang/host/linux-x86
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
     
 # build rom  
@@ -12,4 +11,4 @@ export BUILD_HOSTNAME=lang
 export KBUILD_BUILD_USER=lang    
 export KBUILD_BUILD_HOST=lang
 export TZ=Asia/Jakarta #put before last build command 
-m -jX
+m bacon
