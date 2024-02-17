6342c8b9059598aa9104facb908f91803de23c8c
kitw4y <91876095+kitw4y@users.noreply.github.com>
2024-02-17 16:15:51 +0700

    Update aosp.sh

--- aosp.sh
+++ aosp.sh
@@ -1,4 +1,7 @@
 # sync rom
+repo init -u https://github.com/kitw4y/manifest.git -b 14.0 --git-lfs
+repo sync -c --force-sync --no-tags --no-clone-bundle -j4 --optimized-fetch --prune
+repo sync -j1 --fail-fast
 git clone https://github.com/kitw4y/DeviceTree.git -b syberia-14 device/xiaomi/lancelot
 git clone https://gitlab.com/MT6768Lab/CommonDeviceTree.git -b 13 device/xiaomi/mt6768-common
 git clone https://gitlab.com/MT6768Lab/KernelTree.git -b 13 kernel/xiaomi/mt6768
@@ -8,8 +11,11 @@ git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-20 h
 git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 prebuilts/clang/host/linux-x86
     
 # build rom  
+source build/envsetup.sh
+lunch syberia_lancelot-eng
 export BUILD_USERNAME=lang
 export BUILD_HOSTNAME=lang            
 export KBUILD_BUILD_USER=lang    
 export KBUILD_BUILD_HOST=lang
 export TZ=Asia/Jakarta #put before last build command 
+mka bacon
