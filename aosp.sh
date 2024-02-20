32218c0b41bd1962268dfd0f82521e11928d32b7
kitw4y <91876095+kitw4y@users.noreply.github.com>
2024-02-20 19:24:27 +0700

    test

--- aosp.sh
+++ aosp.sh
@@ -1,17 +1,20 @@
 # sync rom
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
+repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs --depth=1
 git clone https://github.com/kitw4y/local_manifest.git --depth 1 -b RisingOS-14 .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+rm -rf prebuilts/clang/host/linux-x86/clang-latest external/chromium-webview
+repo sync -c -j4 --force-sync --no-clone-bundle --no-tags
     
 # build rom  
 rm -rf toolchain/pgo-profiles
 git clone https://android.googlesource.com/toolchain/pgo-profiles toolchain/pgo-profiles
 sudo apt install ccache -y
-. build/envsetup.sh 
-lunch rising_lancelot-userdebug   
+source build/envsetup.sh 
+opt_patch
+riseup lancelot userdebug
+croot
 export BUILD_USERNAME=lang
 export BUILD_HOSTNAME=lang            
 export KBUILD_BUILD_USER=lang    
 export KBUILD_BUILD_HOST=lang
 export TZ=Asia/Jakarta #put before last build command 
-m bacon
+ascend
