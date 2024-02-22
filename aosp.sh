917791427a5cb244a62c78c27dc24ff940ee29bd
kitw4y <91876095+kitw4y@users.noreply.github.com>
2024-02-22 09:49:36 +0700

    Update aosp.sh

--- aosp.sh
+++ aosp.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs --depth=1
-git clone https://github.com/kitw4y/local_manifest.git --depth 1 -b RisingOS-14 .repo/local_manifests
+git clone https://github.com/syberia-project/manifest.git --depth 1 -b 14.0 .repo/local_manifests
 rm -rf prebuilts/clang/host/linux-x86/clang-latest external/chromium-webview
 repo sync -c -j4 --force-sync --no-clone-bundle --no-tags
     
@@ -10,12 +10,10 @@ git clone https://android.googlesource.com/toolchain/pgo-profiles toolchain/pgo-
 sudo apt update
 sudo apt install ccache -y
 source build/envsetup.sh 
-opt_patch
-riseup lancelot userdebug
-croot
+lunch syberia_lancelot-userdebug
 export BUILD_USERNAME=lang
 export BUILD_HOSTNAME=lang            
 export KBUILD_BUILD_USER=lang    
 export KBUILD_BUILD_HOST=lang
 export TZ=Asia/Jakarta #put before last build command 
-ascend
+mka syberia
