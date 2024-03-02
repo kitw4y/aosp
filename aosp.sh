3ca13541329e1387524ebea211a589cc7926b85c
kitw4y <91876095+kitw4y@users.noreply.github.com>
2024-03-02 07:17:38 +0700

    Update aosp.sh

--- aosp.sh
+++ aosp.sh
@@ -1,10 +1,10 @@
 rm -rf .repo/local_manifests
 rm -rf lsng 
-repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
+repo init -u https://github.com/LineageOS/android.git -b lineage-21.0 --git-lfs
 rm -rf prebuilts/clang/host/linux-x86/clang-latest external/chromium-webview
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 repo sync -j15 --fail-fast
-git clone https://github.com/kitw4y/DeviceTree.git -b RisingOS-14 device/xiaomi/lancelot
+git clone https://github.com/kitw4y/DeviceTree.git -b crDroid-14 device/xiaomi/lancelot
 git clone https://gitlab.com/MT6768Lab/CommonDeviceTree.git -b 14 device/xiaomi/mt6768-common
 git clone https://gitlab.com/MT6768Lab/KernelTree.git -b 13 kernel/xiaomi/mt6768
 git clone https://gitlab.com/MT6768Lab/CommonVendorTree.git -b 14 vendor/xiaomi/mt6768-common
@@ -19,12 +19,10 @@ git clone https://android.googlesource.com/toolchain/pgo-profiles toolchain/pgo-
 sudo apt update
 sudo apt install ccache -y
 source build/envsetup.sh 
-opt_patch
-riseup lancelot userdebug
-croot
+lunch lineage_lancelot-eng
 export BUILD_USERNAME=lang
 export BUILD_HOSTNAME=lang            
 export KBUILD_BUILD_USER=lang    
 export KBUILD_BUILD_HOST=lang
 export TZ=Asia/Jakarta #put before last build command 
-ascend
+m bacon
