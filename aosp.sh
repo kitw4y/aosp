b4b8c81400418b9119ce44d63adba3eea94b1ea5
kitw4y <91876095+kitw4y@users.noreply.github.com>
2024-03-06 16:32:08 +0700

    Update aosp.sh

--- aosp.sh
+++ aosp.sh
@@ -1,9 +1,10 @@
 rm -rf .repo/local_manifests
 rm -rf lsng 
-repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen-qpr
+repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
 rm -rf prebuilts/clang/host/linux-x86/clang-latest external/chromium-webview
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 repo sync -j15 --fail-fast
+sudo rm -rf frameworks/av
 sudo rm -rf device/xiaomi/lancelot
 sudo rm -rf device/xiaomi/mt6768-common
 sudo rm -rf kernel/xiaomi/mt6768
@@ -12,7 +13,8 @@ sudo rm -rf vendor/xiaomi/lancelot
 sudo rm -rf device/mediatek/sepolicy_vndr
 sudo rm -rf hardware/mediatek
 sudo rm -rf hardware/xiaomi
-git clone https://github.com/kitw4y/device_xiaomi_lancelot.git -b cipher device/xiaomi/lancelot
+git clone https://github.com/RisingTechOSS/android_frameworks_av.git -b fourteen frameworks/av
+git clone https://github.com/kitw4y/device_xiaomi_lancelot.git -b 14 device/xiaomi/lancelot
 git clone https://github.com/SuperiorOS-Devices/device_xiaomi_mt6768-common.git -b fourteen device/xiaomi/mt6768-common
 git clone https://github.com/Projects-Corruption-Succubus/HM6768.git kernel/xiaomi/mt6768
 git clone https://github.com/SuperiorOS-Devices/vendor_xiaomi_mt6768-common.git -b fourteen vendor/xiaomi
@@ -24,10 +26,12 @@ git clone https://github.com/Projects-Corruption-Succubus/android_hardware_xiaom
 sudo apt update
 sudo apt install ccache -y
 source build/envsetup.sh 
-lunch cipher_lancelot-userdebug
+opt_patch
+riseup lancelot userdebug
+croot
 export BUILD_USERNAME=lang
 export BUILD_HOSTNAME=lang            
 export KBUILD_BUILD_USER=lang    
 export KBUILD_BUILD_HOST=lang
 export TZ=Asia/Jakarta #put before last build command 
-mka bacon
+ascend
