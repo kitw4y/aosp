fb6b66691924c3a2cb69016e3f704e423db24b2f
kitw4y <91876095+kitw4y@users.noreply.github.com>
2024-03-07 11:15:35 +0700

    Update aosp.sh

--- aosp.sh
+++ aosp.sh
@@ -1,5 +1,9 @@
 rm -rf .repo/local_manifests
 rm -rf lsng 
+sudo rm -rf .repo
+sudo rm -rf *
+sudo rm -rf /tmp/src/android/.repo
+sudo rm -rf /tmp/src/android/*
 repo init -u https://github.com/CipherOS/android_manifest.git -b fourteen-qpr
 rm -rf prebuilts/clang/host/linux-x86/clang-latest external/chromium-webview
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
