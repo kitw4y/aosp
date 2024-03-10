d0a417eb94e344def6b247b12e108fa96250d932
kitw4y <91876095+kitw4y@users.noreply.github.com>
2024-03-10 08:34:42 +0700

    Update aosp.sh

--- aosp.sh
+++ aosp.sh
@@ -1,11 +1,8 @@
-sudo apt install ccache -y
 source build/envsetup.sh
-opt_patch
-riseup lancelot eng
-croot
+lunch cipher_lancelot-ap1a-userdebug
 export BUILD_USERNAME=lang
 export BUILD_HOSTNAME=lang            
 export KBUILD_BUILD_USER=lang    
 export KBUILD_BUILD_HOST=lang
 export TZ=Asia/Jakarta #put before last build command 
-ascend
+mka bacon
