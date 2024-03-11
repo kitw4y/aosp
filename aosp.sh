4908d0756243599aa3bbb6378302e4d2faf7d873
kitw4y <91876095+kitw4y@users.noreply.github.com>
2024-03-11 11:30:17 +0700

    Update aosp.sh

--- aosp.sh
+++ aosp.sh
@@ -1,5 +1,5 @@
 source build/envsetup.sh
-lunch cipher_lancelot-ap1a-userdebug
+lunch cipher_lava-ap1a-userdebug
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export INLINE_KERNEL_BUILDING=true
 export BUILD_USERNAME=lang
