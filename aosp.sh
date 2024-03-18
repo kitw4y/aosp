de5d1c3f1585b598d4e38607f1fcc400ac061b2d
kitw4y <91876095+kitw4y@users.noreply.github.com>
2024-03-18 20:04:16 +0700

    Update aosp.sh

--- aosp.sh
+++ aosp.sh
@@ -1,19 +1,33 @@
-# sync rom
-repo init -u https://github.com/syberia-project/manifest.git -b 14.0 --git-lfs --depth=1
-git clone https://github.com/kitw4y/local_manifest.git --depth 1 -b RisingOS-14 .repo/local_manifests
-rm -rf prebuilts/clang/host/linux-x86/clang-latest external/chromium-webview
-repo sync -c -j4 --force-sync --no-clone-bundle --no-tags
+rm -rf .repo/local_manifests
+rm -rf lsng 
+sudo rm -rf device/xiaomi/lancelot
+sudo rm -rf device/xiaomi/mt6768-common
+sudo rm -rf kernel/xiaomi/mt6768
+sudo rm -rf vendor/xiaomi/mt6768-common
+sudo rm -rf vendor/xiaomi/lancelot
+sudo rm -rf vendor/xiaomi
+sudo rm -rf device/mediatek/sepolicy_vndr
+sudo rm -rf hardware/mediatek
+sudo rm -rf hardware/xiaomi
+git clone https://github.com/kitw4y/DeviceTree.git -b RisingOS-14 device/xiaomi/lancelot
+git clone https://gitlab.com/MT6768Lab/CommonDeviceTree.git -b 14 device/xiaomi/mt6768-common
+git clone https://gitlab.com/MT6768Lab/KernelTree.git -b 13 kernel/xiaomi/mt6768
+git clone https://gitlab.com/MT6768Lab/CommonVendorTree.git -b 14 vendor/xiaomi/mt6768-common
+git clone https://gitlab.com/MT6768Lab/VendorTree.git -b 14 vendor/xiaomi/lancelot
+git clone https://github.com/orkunergun/android_device_mediatek_sepolicy_vndr -b lineage-21 device/mediatek/sepolicy_vndr
+git clone https://github.com/orkunergun/android_hardware_mediatek -b lineage-21 hardware/mediatek
+git clone https://github.com/orkunergun/android_hardware_xiaomi -b lineage-21 hardware/xiaomi
     
 # build rom  
-rm -rf toolchain/pgo-profiles
-git clone https://android.googlesource.com/toolchain/pgo-profiles toolchain/pgo-profiles
 sudo apt update
 sudo apt install ccache -y
 source build/envsetup.sh 
-lunch syberia_lancelot-userdebug
+opt_patch
+riseup lancelot userdebug
+croot
 export BUILD_USERNAME=lang
 export BUILD_HOSTNAME=lang            
 export KBUILD_BUILD_USER=lang    
 export KBUILD_BUILD_HOST=lang
 export TZ=Asia/Jakarta #put before last build command 
-mka syberia
+ascend
