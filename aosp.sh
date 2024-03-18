d58ead3affa1871f4785a96ff0be4a5e1521212a
kitw4y <91876095+kitw4y@users.noreply.github.com>
2024-03-19 04:09:07 +0700

    Update aosp.sh

--- aosp.sh
+++ aosp.sh
@@ -1,6 +1,7 @@
 rm -rf .repo/local_manifests
 rm -rf lsng 
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+repo sync -j15 --fail-fast
 sudo rm -rf device/xiaomi/lancelot
 sudo rm -rf device/xiaomi/mt6768-common
 sudo rm -rf kernel/xiaomi/mt6768
