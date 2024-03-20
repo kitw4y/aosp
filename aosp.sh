3539776b146b210a662f6d5855cce7e4bac1831c
kitw4y <91876095+kitw4y@users.noreply.github.com>
2024-03-20 08:42:18 +0700

    Update aosp.sh

--- aosp.sh
+++ aosp.sh
@@ -2,12 +2,12 @@
 git config --global user.email lang@mail.com
 git config --global user.name lang
 git config --global --add safe.directory /tmp/src/android/development
-source build/envsetup.sh 
-lunch rising_lancelot-userdebug
+. build/envsetup.sh 
+riseup lancelot userdebug
 export BUILD_USERNAME=lang
 export BUILD_HOSTNAME=lang            
 export KBUILD_BUILD_USER=lang    
 export KBUILD_BUILD_HOST=lang
 export TZ=Asia/Jakarta #put before last build command 
 export ALLOW_MISSING_DEPENDENCIES=true
-mka bacon
+ascend
