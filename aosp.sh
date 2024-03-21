57cf615088d5644f0731053cabc2c1cce8cb2ff6
kitw4y <91876095+kitw4y@users.noreply.github.com>
2024-03-21 07:42:24 +0700

    Update aosp.sh

--- aosp.sh
+++ aosp.sh
@@ -1,6 +1,13 @@
-repo sync -j15 --fail-fast
+git clone https://github.com/kitw4y/DeviceTree.git -b crDroid-14 device/xiaomi/lancelot
+git clone https://gitlab.com/MT6768Lab/CommonDeviceTree.git -b 14 device/xiaomi/mt6768-common
+git clone https://gitlab.com/MT6768Lab/KernelTree.git -b 13 kernel/xiaomi/mt6768
+git clone https://gitlab.com/MT6768Lab/CommonVendorTree.git -b 14 vendor/xiaomi/mt6768-common
+git clone https://gitlab.com/MT6768Lab/VendorTree.git -b 14 vendor/xiaomi/lancelot
+git clone https://github.com/orkunergun/android_device_mediatek_sepolicy_vndr -b lineage-21 device/mediatek/sepolicy_vndr
+git clone https://github.com/orkunergun/android_hardware_mediatek -b lineage-21 hardware/mediatek
+git clone https://github.com/orkunergun/android_hardware_xiaomi -b lineage-21 hardware/xiaomi
 source build/envsetup.sh
-lunch cipher_lancelot-userdebug
+lunch lineage_lancelot-userdebug
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export INLINE_KERNEL_BUILDING=true
 export BUILD_USERNAME=lang
